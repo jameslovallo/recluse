f5635782792888dbc142e9069126af3483494740
James Lovallo <james.lovallo@thrivent.com>
2024-05-06 20:10:02 -0400

    add browser-sync output

--- bin/dev.mjs
+++ bin/dev.mjs
@@ -5,6 +5,7 @@ import { execSync } from 'node:child_process'
 export const dev = () => {
 	console.log('Starting Dev Server')
 	execSync(
-		"browser-sync dist --watch & nodemon -e js,css,html --watch src --watch pages --exec 'npm run build'"
+		"browser-sync dist --watch & nodemon -e js,css,html --watch src --watch pages --exec 'npm run build'",
+		{ stdio: 'inherit' }
 	)
 }
