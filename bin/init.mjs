34f15767f60f40df1fd5f60ba356b738f219f960
James Lovallo <james.lovallo@thrivent.com>
2024-08-27 11:50:09 -0400

    test dirname

--- bin/init.mjs
+++ bin/init.mjs
@@ -1,18 +1,23 @@
-import fs from 'fs'
+// import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export const init = () => {
-	fs.cp('../starters/basic/package.json', '../../../package.json', (err) => {
-		err && console.error(err)
-	})
-	fs.cp(
-		'../starters/basic/pages',
-		'../../../pages',
-		{ recursive: true },
-		(err) => {
-			err && console.error(err)
-		}
-	)
-	fs.cp('../starters/basic/src', '../../../src', { recursive: true }, (err) => {
-		err && console.error(err)
-	})
+	console.log(__dirname)
+	// fs.cp('../starters/basic/package.json', '../../../package.json', (err) => {
+	// 	err && console.error(err)
+	// })
+	// fs.cp(
+	// 	'../starters/basic/pages',
+	// 	'../../../pages',
+	// 	{ recursive: true },
+	// 	(err) => {
+	// 		err && console.error(err)
+	// 	}
+	// )
+	// fs.cp('../starters/basic/src', '../../../src', { recursive: true }, (err) => {
+	// 	err && console.error(err)
+	// })
 }
