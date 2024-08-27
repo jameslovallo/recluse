dcf15698dfd6781e1bad9091dcdbaf31dbd2aaa5
James Lovallo <james.lovallo@thrivent.com>
2024-08-27 11:21:53 -0400

    add init script

--- bin.js
+++ bin.js
@@ -2,19 +2,14 @@
 import { build } from './bin/build.mjs'
 import { component } from './bin/component.mjs'
 import { dev } from './bin/dev.mjs'
+import { init } from './bin/init.mjs'
 import { page } from './bin/page.mjs'
 
 const args = process.argv.slice(2)
 
+if (args[0] === 'init') init()
 if (args[0] === 'build') build()
 if (args[0] === 'dev') dev()
-if (args[0] === 'page') {
-	if (args.length === 2 && args[1]) {
-		page(args[1])
-	} else {
-		console.log('Usage: cuirk page <path>')
-	}
-}
 if (args[0] === 'page') {
 	if (args.length === 2 && args[1]) {
 		page(args[1])
--- bin/init.mjs
+++ bin/init.mjs
@@ -1,16 +1,18 @@
 import fs from 'fs'
 
-fs.cp('./starters/basic/package.json', '../../../package.json', (err) => {
-	err && console.error(err)
-})
-fs.cp(
-	'./starters/basic/pages',
-	'../../../pages',
-	{ recursive: true },
-	(err) => {
+export const init = () => {
+	fs.cp('./starters/basic/package.json', '../../../package.json', (err) => {
 		err && console.error(err)
-	}
-)
-fs.cp('./starters/basic/src', '../../../src', { recursive: true }, (err) => {
-	err && console.error(err)
-})
+	})
+	fs.cp(
+		'./starters/basic/pages',
+		'../../../pages',
+		{ recursive: true },
+		(err) => {
+			err && console.error(err)
+		}
+	)
+	fs.cp('./starters/basic/src', '../../../src', { recursive: true }, (err) => {
+		err && console.error(err)
+	})
+}
