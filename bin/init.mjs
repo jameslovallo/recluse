0430a41735e043924438e46380d5f9d1c14dc6fd
James Lovallo <james.lovallo@thrivent.com>
2024-08-27 11:56:51 -0400

    .

--- bin/init.mjs
+++ bin/init.mjs
@@ -3,21 +3,17 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const cwd = process.cwd()
 
 export const init = () => {
 	const starterPath = __dirname.replace('bin', 'starters/basic')
-	fs.cp(`${starterPath}/package.json`, '../../../package.json', (err) => {
+	fs.cp(`${starterPath}/package.json`, `${cwd}/package.json`, (err) => {
 		err && console.error(err)
 	})
-	fs.cp(
-		`${starterPath}/pages`,
-		'../../../pages',
-		{ recursive: true },
-		(err) => {
-			err && console.error(err)
-		}
-	)
-	fs.cp(`${starterPath}/src`, '../../../src', { recursive: true }, (err) => {
+	fs.cp(`${starterPath}/pages`, `${cwd}/pages`, { recursive: true }, (err) => {
+		err && console.error(err)
+	})
+	fs.cp(`${starterPath}/src`, `${cwd}/src`, { recursive: true }, (err) => {
 		err && console.error(err)
 	})
 }
