610df669fe95f439b7d15e17e711994ef4107d27
James Lovallo <james.lovallo@thrivent.com>
2024-08-27 11:55:35 -0400

    .

--- bin/init.mjs
+++ bin/init.mjs
@@ -1,25 +1,23 @@
-// import fs from 'fs'
+import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export const init = () => {
-	console.log(__dirname)
 	const starterPath = __dirname.replace('bin', 'starters/basic')
-	console.log(starterPath)
-	// fs.cp('../starters/basic/package.json', '../../../package.json', (err) => {
-	// 	err && console.error(err)
-	// })
-	// fs.cp(
-	// 	'../starters/basic/pages',
-	// 	'../../../pages',
-	// 	{ recursive: true },
-	// 	(err) => {
-	// 		err && console.error(err)
-	// 	}
-	// )
-	// fs.cp('../starters/basic/src', '../../../src', { recursive: true }, (err) => {
-	// 	err && console.error(err)
-	// })
+	fs.cp(`${starterPath}/package.json`, '../../../package.json', (err) => {
+		err && console.error(err)
+	})
+	fs.cp(
+		`${starterPath}/pages`,
+		'../../../pages',
+		{ recursive: true },
+		(err) => {
+			err && console.error(err)
+		}
+	)
+	fs.cp(`${starterPath}/src`, '../../../src', { recursive: true }, (err) => {
+		err && console.error(err)
+	})
 }
