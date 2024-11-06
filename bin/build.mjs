260b1592dedb293c30a18dd8c5d5b07cd2f74979
James Lovallo <lovallo.james@gmail.com>
2024-11-06 04:31:08 +0000

    - add support for optional custom layouts - move pages dir inside src

--- bin/build.mjs
+++ bin/build.mjs
@@ -1,67 +1,71 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
-import { Marked } from 'marked'
-import { gfmHeadingId } from 'marked-gfm-heading-id'
-import { markedHighlight } from 'marked-highlight'
-import path from 'path'
-import prism from 'prismjs'
+import fs from "fs";
+import { Marked } from "marked";
+import { gfmHeadingId } from "marked-gfm-heading-id";
+import { markedHighlight } from "marked-highlight";
+import path from "path";
+import prism from "prismjs";
 
 const marked = new Marked(
 	markedHighlight({
-		langPrefix: 'language-',
+		langPrefix: "language-",
 		highlight(code, lang) {
 			if (prism.languages[lang]) {
-				return prism.highlight(code, prism.languages[lang], lang)
+				return prism.highlight(code, prism.languages[lang], lang);
 			} else {
-				return code
+				return code;
 			}
 		},
 	})
-).use(gfmHeadingId())
+).use(gfmHeadingId());
 
 const writePage = async (filename) => {
-	const layout = await import('../../../src/layouts/default.js').then(
+	console.log(filename);
+	const defaultLayout = await import("../../../src/layouts/default.js").then(
 		(m) => m.default
-	)
-	import('../../../' + filename).then(({ meta, body }) => {
-		const page = layout({
-			meta,
-			children: marked.parse(body),
-		})
-		if (filename === 'pages/home.js') {
-			filename = 'pages'
+	);
+	import("../../../" + filename).then(
+		({ meta, body, layout: providedLayout }) => {
+			const layout = providedLayout || defaultLayout;
+			const page = layout({
+				meta,
+				children: marked.parse(body),
+			});
+			if (filename === "src/pages/home.js") {
+				filename = "dist";
+			}
+			const outDir = filename.replace("src/pages", "dist").replace(".js", "");
+			fs.mkdirSync(outDir, { recursive: true });
+			fs.writeFileSync(outDir + "/index.html", page, {
+				encoding: "utf8",
+			});
 		}
-		const outDir = filename.replace('pages', 'dist').replace('.js', '')
-		fs.mkdirSync(outDir, { recursive: true })
-		fs.writeFileSync(outDir + '/index.html', page, {
-			encoding: 'utf8',
-		})
-	})
-}
+	);
+};
 
 const writePages = (startPath, filter) => {
-	console.log('Building...')
+	console.log("Building...");
 	if (!fs.existsSync(startPath)) {
-		console.log('directory does not exist: ', startPath)
-		return
+		console.log("directory does not exist: ", startPath);
+		return;
 	}
-	const files = fs.readdirSync(startPath, { recursive: true })
+	const files = fs.readdirSync(startPath, { recursive: true });
 	for (var i = 0; i < files.length; i++) {
-		var filename = path.join(startPath, files[i])
+		var filename = path.join(startPath, files[i]);
 		if (filename.endsWith(filter)) {
-			writePage(filename)
+			writePage(filename);
 		}
 	}
-}
+};
 
 export const build = () => {
-	fs.rmSync('./dist', { recursive: true, force: true })
-	fs.mkdirSync('./dist')
+	fs.rmSync("./dist", { recursive: true, force: true });
+	fs.mkdirSync("./dist");
 
-	writePages('./pages', '.js')
+	writePages("./src/pages", ".js");
 
-	fs.cp('./src', './dist/src', { recursive: true }, (err) => {
-		err && console.error(err)
-	})
-}
+	fs.cp("./src", "./dist/src", { recursive: true }, (err) => {
+		err && console.error(err);
+	});
+};
