d85610ee94b1e78821cee4816df242f9e46b1c18
jameslovallo <lovallo.james@gmail.com>
2024-11-15 09:15:24 -0500

    remove docs

--- docs/src/pages/home.js
+++
@@ -1,8 +0,0 @@
-import { md } from "cuirk";
-import meta from "../data/meta.js";
-
-export const body = md`
-# ${meta.title}
-
-Cuirk is a static site generator and web component framework focused on performance, accessibility, and simplicity. It's a rejection of the chaos and complexity of modern web development.
-`;
