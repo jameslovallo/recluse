7ce40bd6526135caf2abd78d3d90cc0687ace296
James Lovallo <lovallo.james@gmail.com>
2024-11-28 21:14:54 -0500

    update built in component helpers

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import fs from "fs";
 import { compileString } from "sass";
 
 const combine = (strings, ...values) => {
-	return strings
-		.map((string, i) => string + (values[i] || ""))
-		.join("")
-		.trim();
+  return strings
+    .map((string, i) => string + (values[i] || ""))
+    .join("")
+    .trim();
 };
 
 export const html = combine;
@@ -15,114 +15,231 @@ export const scss = combine;
 export const sass = combine;
 
 export const loop = (arr, template) => {
-	if (typeof arr === "number") {
-		arr = [...Array(arr)];
-	}
-	return arr
-		.map((el, index) => {
-			if (typeof template === "function") {
-				return template({ ...el, index });
-			} else return template;
-		})
-		.join("");
+  if (typeof arr === "number") {
+    arr = [...Array(arr)];
+  }
+  return arr
+    .map((el, index) => {
+      if (typeof template === "function") {
+        return template({ ...el, index });
+      } else return template;
+    })
+    .join("");
 };
 
 export const renderChildren = (children) =>
-	Array.isArray(children) ? children.join("") : children;
+  Array.isArray(children) ? children.join("") : children;
 
 export const when = (condition, html) => (condition ? html : "<!-- -->");
 
 export const componentStyles = (components) => html`
-	<style>
-		:root {
-			--c-primary: #1e90ff;
-			--c-primary-1: #1e90ff11;
-			--c-primary-2: #1e90ff22;
-			--c-primary-3: #1e90ff33;
-			--c-primary-hover: var(--c-primary-2);
-			--c-primary-active: var(--c-primary-3);
-			--c-primary-color: white;
-			--c-border: 1px solid #8888;
-			--c-surface: white;
-			--c-surface-color: black;
-			--c-surface-shadow: rgba(0, 0, 0, 0.08) 0px 2px 4px;
-		}
+  <style>
+    :root {
+    	font-family: sans-serif;
+    	--c-dark: black;
+    	--c-light: white;
+    	--c-neutral: #8888;
+    	--c-neutral-light: #8882;
+    	--c-primary: #2962ff;
+    	--c-on-primary: white;
+    	--c-secondary: #6200ea;
+    	--c-on-secondary: white;
+    	--c-success: #00c853;
+    	--c-on-success: white;
+    	--c-error: #d50000;
+    	--c-on-error: white;
+    	--c-warning: #ffd600;
+    	--c-on-warning: black;
+    	--c-surface: #8881;
+    }
 
-		:root,
-		body {
-			height: 100%;
-		}
+    * {
+    	box-sizing: border-box;
+    }
 
-		body {
-			margin: 0;
-		}
+    .c-square,
+    .c-circle {
+    	align-items: center;
+    	aspect-ratio: 1;
+    	display: inline-flex;
+    	justify-content: center;
+    	vertical-align: middle;
+    }
 
-		* {
-			box-sizing: border-box;
-		}
+    .c-circle {
+    	border-radius: 50%;
+    }
 
-		img,
-		video {
-			display: block;
-			max-width: 100%;
-		}
+    .c-rounded {
+    	border-radius: var(--c-rounded-radius, 0.25rem);
+    }
 
-		svg {
-			display: block;
-		}
+    .c-pill {
+    	border-radius: var(--c-pill-radius, 50vh);
+    }
 
-		.surface {
-			background: var(--c-surface);
-			border: var(--c-border);
-			color: var(--c-surface-color);
-		}
+    .c-row {
+    	display: flex;
+    	gap: 0.5rem;
+    }
 
-		${compileString(
-			Object.values(components)
-				.map((component) => component.style)
-				.join("\n")
-		).css}
-	</style>
+    .c-column {
+    	display: flex;
+    	flex-direction: column;
+    	gap: 0.5rem;
+    }
+
+    .c-stack {
+    	display: inline-grid;
+    }
+
+    .c-stack > * {
+    	grid-area: 1/-1;
+    	width: 100%;
+    }
+
+    .c-center {
+    	align-items: center;
+    	display: flex;
+    	justify-content: center;
+    }
+
+    .c-light {
+    	color: var(--c-dark);
+    }
+
+    .c-dark {
+    	color: var(--c-light);
+    }
+
+    .c-primary {
+    	background: var(--c-primary);
+    	color: var(--c-on-primary);
+    }
+
+    .c-primary.invert {
+    	background: var(--c-on-primary);
+    	color: var(--c-primary);
+    }
+
+    .c-secondary {
+    	background: var(--c-secondary);
+    	color: var(--c-on-secondary);
+    }
+
+    .c-secondary.invert {
+    	background: var(--c-on-secondary);
+    	color: var(--c-secondary);
+    }
+
+    .c-success {
+    	background: var(--c-success);
+    	color: var(--c-on-success);
+    }
+
+    .c-success.invert {
+    	background: var(--c-on-success);
+    	color: var(--c-success);
+    }
+
+    .c-error {
+    	background: var(--c-error);
+    	color: var(--c-on-error);
+    }
+
+    .c-error.invert {
+    	background: var(--c-on-error);
+    	color: var(--c-error);
+    }
+
+    .c-warning {
+    	background: var(--c-warning);
+    	color: var(--c-on-warning);
+    }
+
+    .c-warning.invert {
+    	background: var(--c-on-warning);
+    	color: var(--c-warning);
+    }
+
+    .c-surface {
+    	background: var(--c-surface, var(--c-neutral-light));
+    	position: relative;
+    }
+
+    .c-action {
+    	cursor: pointer;
+    	position: relative;
+    }
+    .c-action:before {
+    	background: var(--c-action, var(--c-primary));
+    	bottom: 0;
+    	content: "";
+    	left: 0;
+    	opacity: 0;
+    	position: absolute;
+    	right: 0;
+    	top: 0;
+    }
+
+    .c-action:hover:before {
+    	opacity: 0.15;
+    }
+
+    .c-action:active:before {
+    	opacity: 0.3;
+    }
+
+    .c-border {
+    	border: 1px solid var(--c-neutral);
+    }
+
+
+    ${compileString(
+      Object.values(components)
+        .map((component) => component.style)
+        .join("\n")
+    ).css}
+  </style>
 `;
 
 export const componentScripts = (components) =>
-	Object.values(components)
-		.map((component) => {
-			if (component.init) {
-				const scriptArr = String(component.init).split("\n");
-				scriptArr.shift() && scriptArr.pop();
-				return `<script>${scriptArr.join("\n")}</script>`;
-			}
-		})
-		.join("\n");
+  Object.values(components)
+    .map((component) => {
+      if (component.init) {
+        const scriptArr = String(component.init).split("\n");
+        scriptArr.shift() && scriptArr.pop();
+        return `<script>${scriptArr.join("\n")}</script>`;
+      }
+    })
+    .join("\n");
 
 export const createTags = (data, type) => {
-	return !!data
-		? data
-				.map((tag) => {
-					const attrs = Object.keys(tag)
-						.map((key) => `${key}="${tag[key]}"`)
-						.join(" ");
-					if (type === "script") {
-						return `<script ${attrs}></script>`;
-					} else if (type === "link") {
-						return `<link ${attrs}>`;
-					}
-				})
-				.join("\n")
-		: "";
+  return !!data
+    ? data
+        .map((tag) => {
+          const attrs = Object.keys(tag)
+            .map((key) => `${key}="${tag[key]}"`)
+            .join(" ");
+          if (type === "script") {
+            return `<script ${attrs}></script>`;
+          } else if (type === "link") {
+            return `<link ${attrs}>`;
+          }
+        })
+        .join("\n")
+    : "";
 };
 
 export const embedCode = (file) =>
-	[
-		"```" + file.split(".").pop(),
-		fs.readFileSync(file, "utf-8").replaceAll("\t", "  "),
-		"```",
-	].join("\n");
-
+  [
+    "```" + file.split(".").pop(),
+    fs.readFileSync(file, "utf-8").replaceAll("\t", "  "),
+    "```",
+  ].join("\n");
 
 export const classList = (props) =>
   props
-    .map((prop) => `c-${prop.replaceAll(' ', ' c-')}`)
+    .map((prop) => `c-${prop.replaceAll(" ", " c-")}`)
     .join(" ")
     .trim();
